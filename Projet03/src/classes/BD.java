146cfb39e80dc344d414bac9ebe4afddc7f2a888
Rebecca Dauphnie Mombrun <80013229+starcess@users.noreply.github.com>
2022-05-09 22:16:17 -0400

    Add files via upload

---
+++ Projet03/src/classes/BD.java
@@ -0,0 +1,20 @@
+/* Fichier BD.java
+ * Auteur : rebec
+ * Date de création : May 9, 2022
+ */
+package classes;
+
+/**
+ *  TODO
+ * Author : Prénom, nom
+ */
+public class BD {
+
+	/**
+	 * 
+	 */
+	public BD() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
